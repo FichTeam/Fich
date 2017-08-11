71fbc9427725be248ab5cdc23994594049cae8c4
Triet Nguyen <triet@fossil.com>
2017-08-11 16:31:14 +0700

    - Add simulation cell for Phat

---
+++ Fich/View/SimulateCell.swift
@@ -0,0 +1,24 @@
+//
+//  SimulateCell.swift
+//  Fich
+//
+//  Created by Triet on 8/11/17.
+//  Copyright © 2017 fichteam. All rights reserved.
+//
+
+import UIKit
+
+class SimulateCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
